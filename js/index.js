bb530e0ec8caa56f5e7ed0468bea3e8c82540a08
heba0 <hebah@qatar.cmu.edu>
2017-09-24 19:17:01 +0200

    basic structure

---
+++ js/index.js
@@ -0,0 +1,4 @@
+
+setTimeout(myFunction, 2000);
+function myFunction(){
+      document.getElementById("frontDiv").innerHTML = '<strong >Hi there !</strong><br> This website is still under development, stay tuned for the coming updates<br> -Heba';}
