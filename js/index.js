883b1bccd8b4a8ccd1c46d1608921ec393f3e028
heba0 <hebah@qatar.cmu.edu>
2017-09-24 19:38:54 +0200

    .

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 
 setTimeout(myFunction, 2000);
 function myFunction(){
-      document.getElementById("frontDiv").innerHTML = '<strong >Hi there !</strong><br> This website is still under development, stay tuned for the coming updates<br> -Heba';}
+      document.getElementById("frontDiv").innerHTML = '<strong >Hi there !</strong><br> This website is still under development, please stay tuned for the coming updates <br> -Heba';}
